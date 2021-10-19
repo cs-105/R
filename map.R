85c586ec22fdaca28da633683213784c3c49ff88
Ethan Vaughan <ethandvaughan@gmail.com>
2021-10-18 17:14:49 -0700

    Take out pipe

--- map.R
+++ map.R
@@ -1,5 +1,7 @@
 library(leaflet)
 library(shiny)
   
-map <- leaflet() %>% addTiles() %>% fitBounds(map, -124.848974, 24.396308, -66.885444, 49.384358)
-map 
+map <- leaflet() 
+map <- addTiles(map)
+map <-  fitBounds(map, -124.848974, 24.396308, -66.885444, 49.384358)
+map
