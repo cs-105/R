fade2bfedc77dc349724e5f8b4dbafb5ae2aed0f
justEClaire <70547689+justEClaire@users.noreply.github.com>
2021-11-17 17:19:16 -0800

    updated broken file

--- read-csv.R
+++ read-csv.R
@@ -1,9 +1,9 @@
 readVeg <- function() {
-  return(read.csv(file = "vegetation.csv", row.names = 1, header = TRUE, check.names = FALSE))
+  vegetableFile <- read.csv(file = "vegetation.csv", row.names = 1, header = TRUE, check.names = FALSE)
+  return(vegetableFile)
 }
 
-getVegetation <- function(long, lat) {
-  vegFile <- readVeg()
+getVegetation <- function(long, lat, vegFile) {
   long <- (round_any(long, .1, f = floor) + .05)
   lat <- round_any(lat, .1, f = floor) + .05
   if (long > -64.95 || long < -127 || lat > 50 || lat < 23.4) {
