b85c5ad960b03ac85565ddd71cfbe509cd06b62b
justEClaire <70547689+justEClaire@users.noreply.github.com>
2021-11-29 16:38:17 -0800

    updated code

--- read-csv.R
+++ read-csv.R
@@ -1,9 +1,9 @@
 readVeg <- function() {
-  vegetableFile <- read.csv(file = "vegetation.csv", row.names = 1, header = TRUE, check.names = FALSE)
-  return(vegetableFile)
+  return(read.csv(file = "vegetation.csv", row.names = 1, header = TRUE, check.names = FALSE))
 }
 
-getVegetation <- function(long, lat, vegFile) {
+getVegetation <- function(long, lat) {
+  vegFile <- readVeg()
   long <- (round_any(long, .1, f = floor) + .05)
   lat <- round_any(lat, .1, f = floor) + .05
   if (long > -64.95 || long < -127 || lat > 50 || lat < 23.4) {
