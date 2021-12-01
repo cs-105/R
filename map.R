375e8787897d3c4895aba727d0f85c01622042b5
Ethan Vaughan <ethandvaughan@gmail.com>
2021-11-30 22:56:21 -0800

    Finalize predict functions, comment and clean up code.

--- map.R
+++ map.R
@@ -6,8 +6,9 @@ library(plyr)
 
 source("predict-functions.R")
 
-Sys.setenv(OWM_API_KEY = "ac66c8209bdf887068a2a79e4fdbca33") 
+API_key = "ac66c8209bdf887068a2a79e4fdbca33"
 
+# Initiate variables
 long <- 0.0
 lati <- 0.0
 hour <<- 0
@@ -19,12 +20,16 @@ grid <- c()
 weather_grid <- c()
 directions <- list(list(1,1),list(1,-1),list(-1,1),list(-1,-1),list(1,0),list(0,1),list(-1,0),list(0,-1))
 grid_density <- 100
+content <<- "Fire will not start. 
+            Try a new location."
 
+# Set up the page
 ui <- fluidPage(
   tags$head(
     tags$script(type = "text/javascript"),
     useShinyjs()
   ),
+  titlePanel("Fire Prediction Model In R"),
   fluidRow(
     leafletOutput("map")
   ),
@@ -35,7 +40,19 @@ ui <- fluidPage(
                   value = 1),
     ),
     mainPanel(
-      tableOutput("values")
+      p("Wildfire Simulator 
+        that integrates geography 
+        with current and historical 
+        weather to form a fire prediction 
+        at a selected location around the 
+        United States."),
+      p("To use, click a location on the 
+        map, move the slider to the amount 
+        of hours you would like to predict 
+        for, then click burn. If the number 
+        of hours is large it may take quite 
+        some time to run, so please be 
+        patient. Thank you!")
     )
   ),
   fluidRow(
@@ -44,16 +61,15 @@ ui <- fluidPage(
   )
 )
 
+# Set up the backend server
 server <- function(input, output, session) {
   map <- leaflet() %>%
     addTiles() %>%
     fitBounds(-124.848974, 24.396308, -66.885444, 49.384358)
-    
+  output$map <- renderLeaflet(map)
   
-  output$map <- renderLeaflet(
-    map
-  )
-  
+  # Handle map click event
+  # Adds a marker and stors the coordinates where the mouse is clicked
   observeEvent(input$map_click, {
     id <- paste0("marker", input$map_click)
     click <- input$map_click
@@ -71,22 +87,22 @@ server <- function(input, output, session) {
     lati <<- click$lat
   })
   
+  # Handle 'Burn" button click
+  # Simulates the fire growth for the given number of hours
   observeEvent(input$updateFire, {
-    print("Starting burn")
     
     leafletProxy("map") %>% 
       clearMarkers()
     
     if(willFireStart(long, lati)){
-
       leafletProxy("map") %>%
         addCircles(lng = long, lat = lati, weight = 1, radius = 800, color = "#000000", group = "fires")
-
-      print("Fire is starting")
+      
       hour <<- input$sethour
       grid <<- c()
       burn_area <<- c()
 
+      # Creates a grid surrounding the starting coordinates
       for(i in 1:grid_density){
         row_i <- c()
         for(j in 1:grid_density){
@@ -109,22 +125,18 @@ server <- function(input, output, session) {
       grow_fire(grid, list(list(50,50)),1, hour)
     
       draw_fire()
-    }
-    else {
-      content <- paste(sep = "<br/>",
-                       "Fire will not start.",
-                       "Try a new location."
-      )
-        leafletProxy("map") %>% 
-          addPopups(long, lati, content, options = popupOptions((closeButton = TRUE)))
+    } else {
+      leafletProxy("map") %>% 
+        addPopups(long, lati, content, options = popupOptions((closeButton = TRUE)))
     }
   })
   
   # Grid is the grid built that the fire can spread to
   # spread_squares are the squares on fire that are currently spreading the fire
   # Depth is the depth of the recursion.  How many squares deep will the fire spread to
-  
+  # Hour is the number of hours to predict
   grow_fire <- function(grid, spread_squares, d, hour){
+    
     if(d>ceiling(hour/2)){
       return()
     }
@@ -153,23 +165,19 @@ server <- function(input, output, session) {
     
     burn_area <<- append(burn_area, new_spread_squares)
     
-    print(d)
-    
     wind_speed <<- weather$list[[d+1]]$wind$speed
     wind_dir <<- weather$list[[d+1]]$wind$deg
-
-    print(wind_speed)
-    print(deg_to_NSEW(wind_dir))
     
     grow_fire(grid, new_spread_squares, d+1, hour)
   }
   
+  # Returns True if fire will spread to a new location
+  # Returns False if not
   will_spread <- function(cell1, cell2){
     lon <- cell2[[1]]
     lat <- cell2[[2]]
     vegetation <- getVegetation(lon, lat)
 
-    
     # gets the cell change vector
     x <- cell2[[1]]-cell1[[1]]
     if(x > 0)
@@ -211,6 +219,7 @@ server <- function(input, output, session) {
     if (dd > 180) {
       dd <- 360 - dd
     }
+    # Account for wind speed
     if (dd > 120) {
       wind_prob <- (wind_speed / 2) + log(dd)
     } else if (dd < 60) {
@@ -218,6 +227,7 @@ server <- function(input, output, session) {
     } else {
       wind_prob <- log(dd)
     }
+    # Add randomness to account for the chaotic nature of fire
     rand <- sample(1:10, 1)
     prob = (6 * log(1 + vegetation)) + wind_prob
     
@@ -227,6 +237,7 @@ server <- function(input, output, session) {
     return(TRUE)
   }
   
+  # Iterates through the cells that the fire will spread to and creates the circles
   draw_fire <- function(){
     for(pos in burn_area){
       lon <- grid[[pos[[1]]]][[pos[[2]]]][[1]]
@@ -236,6 +247,7 @@ server <- function(input, output, session) {
     }
   }
   
+  # Converts wind degree to character
   deg_to_NSEW <- function(deg){
     dir_list <- list('SW','W','NW','N','NE','E','SE')
     if(wind_dir > 22.5 & wind_dir < 337.5){
@@ -245,6 +257,7 @@ server <- function(input, output, session) {
       return('S')
   }
   
+  # Removes cirles and markers
   observeEvent(input$endFires, {
     
     leafletProxy("map") %>% 
@@ -255,4 +268,5 @@ server <- function(input, output, session) {
   })
 }
 
+# Deploys app
 shinyApp(ui, server)
--- predict-functions.R
+++ predict-functions.R
@@ -3,12 +3,10 @@ library(rjson)
 
 source("read-csv.R")
 
-Sys.setenv(OWM_API_KEY = "ac66c8209bdf887068a2a79e4fdbca33") 
 API_key = "ac66c8209bdf887068a2a79e4fdbca33"
 
+# Returns True if a fire will start at the given coordinates
 willFireStart <- function(long, lat) {
-  print("Enter Predict")
-  
   fireThreshold <- 0
   tryCatch({
     url <-
@@ -30,8 +28,6 @@ willFireStart <- function(long, lat) {
     return(FALSE)
   })
   
-  
-    
   ##RAIN - IF ITS RAINING, RETURN FALSE
   ##RAIN all historical counts 
   rain1 <- historical_weather$list[[1]]$weather[[1]]$main 
@@ -40,6 +36,9 @@ willFireStart <- function(long, lat) {
   rain4 <- historical_weather$list[[4]]$weather[[1]]$main
   if(rain1 == "Rain" || rain2 == "Rain" || rain3 == "Rain" || rain4 == "Rain") { 
     print("Fire will not start (Rain)")
+    content <<- "Fire will not start. 
+                 Too wet. 
+                 Try a new location."
     return(FALSE)
   }
     
@@ -81,19 +80,30 @@ willFireStart <- function(long, lat) {
 
   #ATMOSPHERIC PRESSURE-10.2
   #Conversion: 10.2PSI(10'000 feet fire will not burn) - 689.4757293178 hPa
+  # Returns False if elevation is too high
   if(historical_weather$list[[1]]$main$pressure < 689.4757293178) { 
     print("Fire will not start (Pressure)")
+    print(historical_weather$list[[1]]$main$pressure)
+    content <<- "Fire will not start. 
+                 Elevation is too high. 
+                 Try a new location."
     return(FALSE)
   }
   
+  # Return False if vegetation is too low
   vegetation <- getVegetation(long, lat)
   if(is.null(vegetation) || vegetation < .25) {
     print("Vegetation too low:")
     print(vegetation)
     print("Fire will not start")
+    content <<- "Fire will not start. 
+                 Vegetation is too low. 
+                 Try a new location."
     return(FALSE)
   }
   
+  # Return True if threshold is met
+  # Return False if threshold is too low
   if(fireThreshold >= 1) {
     print("Fire will start")
     print(fireThreshold)
